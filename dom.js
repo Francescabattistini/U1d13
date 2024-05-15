437479dd24f41575744c54fa6d2ccef73df79164
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-05-15 16:24:08 +0200

    a

---
+++ dom.js
@@ -0,0 +1,11 @@
+/* function faiCose(parametroUno, parametroDue) {
+  const area = parametroUno * parametroDue;
+  console.log(area);
+} */
+/* function richiamaIlBottone() {
+  console.log(document.body);
+}
+richiamaIlBottone();  */ //chiama la funzione
+const taskImput = (ev) => {
+  console.log(ev.target.value);
+};
